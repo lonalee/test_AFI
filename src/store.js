fa3d1f1f311d8a3f9dcf9d02027b2f0f10c4a99e
lonalee2018 <lonalee2018@gmail.com>
2018-11-26 23:52:36 +0900

    edited a little

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ import thunk from "redux-thunk";
 const middleware = [thunk];
 const initialState = {};
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
